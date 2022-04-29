3d6eed518d609909ee98d067546262014ee9de4e
Mack Gallagher <wingsonthebus@gmail.com>
2022-04-28 19:25:43 -0700

    align form items vertically

--- src/App.js
+++ src/App.js
@@ -1,8 +1,58 @@
-import React from "react";
+import './App.css'
+import React, { useState } from "react";
 import { BrowserRouter, Link, Route } from 'react-router-dom';
 import PizzaForm from './PizzaForm.js';
 
+/* VALIDATION */
+import * as yup from 'yup';
+import schema from './validation/schema.js';
+/* ********** */
+
+
 const App = () => {
+
+  const initialFormValues = {
+                              name: '',
+                              size: '',
+                              pepperoni: false,
+                              sausage: false,
+                              olives: false,
+                              onions: false,
+                              pineapple: false,
+                              peppers: false,
+                              special: '',
+                            };
+  
+  const initialFormErrors = {
+                              name: '',
+                              size: '',
+                              pepperoni: '', 
+                              sausage: '',
+                              olives: '',
+                              onions: '',
+                              pineapple: '',
+                              peppers: '',
+                              special: '',
+                            };
+
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [formErrors, setFormErrors] = useState(initialFormErrors);
+
+  const onChange = evt => {
+    const name = evt.target.name;
+    const value = evt.target.type === 'checkbox' ? evt.target.checked : evt.target.value;
+    validate(name,value);
+    setFormValues({ ...formValues, [name]: value });
+    console.log(formValues);
+  }
+
+  const validate = (name, value) => {
+    yup.reach(schema, name)
+      .validate(value)
+        .then(res => setFormErrors({ ...formErrors, [name]: "" }))
+        .catch(err => setFormErrors({ ...formErrors, [name]: err.errors[0]}))
+  }
+
   return (
     <div className="App">
       <h1>Lambda Eats</h1>
@@ -14,7 +64,13 @@ const App = () => {
         </Route>
        
         <Route path='/pizza'>
-          <PizzaForm />
+          <PizzaForm
+            formValues={formValues}
+            setFormValues={setFormValues}
+            formErrors={formErrors}
+            setFormErrors={setFormErrors}
+            onChange={onChange}
+          />
         </Route>
       </BrowserRouter>
     </div>
